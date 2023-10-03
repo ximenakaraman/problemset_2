1112d57393f55aa8e3af1cb8d8169c158d0eefee
ximenakaraman <x.karaman@uniandes.edu.co>
2023-10-02 21:22:51 -0500

    El codigo hasta el 2 de octubre, 9:22 pm

--- code/script.R
+++ code/script.R
@@ -5,7 +5,13 @@
 #R version 4.3.1 (2023-06-16)
 
 library(pacman)
-p_load(rio, data.table, tidyverse, haven, skimr, janitor)
+p_load(rio, 
+       data.table, 
+       tidyverse, 
+       haven, 
+       skimr, 
+       janitor) #Descargar las bibliotecas necesarias para manipular bases de datos
+
 setwd("~/Library/CloudStorage/OneDrive-UniversidaddelosAndes/2023-2/R/problemset_2/")
 
 #2.1 Importar las bases de datos
@@ -23,24 +29,43 @@ saveRDS(object = identification, file = "identification.rds")
 #3 Generar variables 
 
 #3.1 
-identification <- mutate(identification, bussiness_type=case_when(GRUPOS4==01~"Agricultura", GRUPOS4==02~"Industria_manufacturera", GRUPOS4==03~"Comercio", GRUPOS4==04~"Servicios"))
-view(identification) #REVISAR, no nos toma los valores, solo aparece NA
-identification <- mutate_if(identification, is.character, as.factor) #REVISAR, no sabemos si esto funciona
+identification$GRUPOS4 <- as.numeric(identification$GRUPOS4)
+identification <- mutate(identification, bussiness_type=case_when(GRUPOS4==1~"Agricultura", 
+                                                                  GRUPOS4==2~"Industria_manufacturera", 
+                                                                  GRUPOS4==3~"Comercio", 
+                                                                  GRUPOS4==4~"Servicios"))
 
 #3.2
-location <- mutate(location, local=case_when(P3053==6~"1", P3053==7~"1"))
+location <- mutate(location, local=case_when(P3053==6~"1", 
+                                             P3053==7~"1"))
 view(location)
+
 
 #4 Eliminar filas/columnas de un conjunto de datos
 
 #4.1
-identification_sub <- subset(identification, GRUPOS4=="02")
+identification_sub <- subset(identification, bussiness_type=="Industria_manufacturera")
 
 #4.2
-location_sub <- subset(location, select=c(DIRECTORIO, SECUENCIA_P, SECUENCIA_ENCUESTA, P3054, P469, COD_DEPTO, F_EXP))
+location_sub <- subset(location, select=c(DIRECTORIO, 
+                                          SECUENCIA_P, 
+                                          SECUENCIA_ENCUESTA, 
+                                          P3054, 
+                                          P469, 
+                                          COD_DEPTO, 
+                                          F_EXP))
 
 #4.3
-identification_sub <- subset(identification_sub, select = c(DIRECTORIO, SECUENCIA_P, SECUENCIA_ENCUESTA, P35, P241, P3032_1, P3032_2, P3032_3, P3033, P3034))
+identification_sub <- subset(identification_sub, select = c(DIRECTORIO, 
+                                                            SECUENCIA_P, 
+                                                            SECUENCIA_ENCUESTA, 
+                                                            P35, 
+                                                            P241, 
+                                                            P3032_1, 
+                                                            P3032_2, 
+                                                            P3032_3, 
+                                                            P3033, 
+                                                            P3034))
 
 #5
 location_sub_X_identification_sub <- merge(location_sub, identification_sub, by = c("DIRECTORIO", "SECUENCIA_P", "SECUENCIA_ENCUESTA"))
